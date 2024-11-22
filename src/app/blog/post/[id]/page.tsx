07dda9f8645ab8defff62899b260d2cd39e71d0a
Rumyana Atanasova <rumyana_atanasova@epam.com>
2024-11-22 15:16:45 +0200

    fixed dynamic routing for post

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,15 +1,12 @@
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 
-type tParams = Promise<{ slug: string[] }>;
-
-export default async function Page(props: { params: tParams }) {
-  const { slug } = await props.params;
-  const postID = slug[1];
-  const post = posts.find((post) => post.id === postID);
+export default async function Page({params}:any) {
+  const resolvedParams = await params
+  const post = posts.find((post) => post.id === resolvedParams.id);
   return (
     <>
       <h1>Post</h1>
-      <Post {...post} />
+      {post && <Post {...post} />}
     </>)
 }
